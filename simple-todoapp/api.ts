fff846f4801ff1b401d4e120940b7632a45cdcd6
sunnyheee <fool0116@naver.com>
2023-11-05 01:45:46 +0900

    create todolist

---
+++ simple-todoapp/api.ts
@@ -0,0 +1,39 @@
+import { ITask } from "./types/type";
+
+const baseUrl = "http://localhost:9000";
+
+export const getAllTodos = async (): Promise<ITask[]> => {
+  const res = await fetch(`${baseUrl}/tasks`, { cache: "no-store" });
+  const todos = await res.json();
+  return todos;
+};
+
+export const addTodo = async (todo: ITask): Promise<ITask> => {
+  const res = await fetch(`${baseUrl}/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const newTodo = await res.json();
+  return newTodo;
+};
+
+export const editTodo = async (todo: ITask): Promise<ITask> => {
+  const res = await fetch(`${baseUrl}/tasks/${todo.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const updatedTodo = await res.json();
+  return updatedTodo;
+};
+
+export const deleteTodo = async (id: string): Promise<void> => {
+  await fetch(`${baseUrl}/tasks/${id}`, {
+    method: "DELETE",
+  });
+};
