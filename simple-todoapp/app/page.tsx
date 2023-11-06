b49fb59f7f6826ad72d73fa7c9594b21b7258e49
sunnyheee <fool0116@naver.com>
2023-11-06 23:59:56 +0900

    edit css

--- simple-todoapp/app/page.tsx
+++ simple-todoapp/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const tasks = await getAllTodos();
   console.log(tasks);
   return (
-    <main className="max-w-4xl mx-auto mt-4">
+    <main className="max-w-4xl mx-auto mt-4 w-11/12">
       <div className="text-center my-5 flex flex-col gap-4">
         <h1 className="text-2xl font-bold">Todo List App</h1>
         <AddTask />
